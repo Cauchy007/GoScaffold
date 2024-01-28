d1d3034257d76bf2ef9c03046ef377b7319ce4bd
cauchy <liurui@xiaoduotech.com>
2024-01-28 20:38:08 +0800

    use cobra

--- cmd/root.go
+++ cmd/root.go
@@ -1,21 +1,29 @@
 package cmd
 
 import (
+	"context"
 	"fmt"
+	"log"
+
 	"github.com/spf13/cobra"
 )
 
-var serveCmd = &cobra.Command{
+var rootCmd = &cobra.Command{
 	Use:   "serve",
 	Short: "Start the server",
 	Long:  `Start the server for your application`,
+	PreRun: func(cmd *cobra.Command, args []string) {
+		fmt.Println("Starting the server:[pre-run]...")
+	},
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("Starting the server...")
+		fmt.Println("Starting the server:[run]...")
 		// Add your server start logic here
 	},
 }
 
-func init() {
-	// Register the serve command with the root command
-	rootCmd.AddCommand(serveCmd)
+func Execute() error {
+	if err := rootCmd.Execute(); err != nil {
+		log.Fatalln(context.Background(), err)
+	}
+	return nil
 }
